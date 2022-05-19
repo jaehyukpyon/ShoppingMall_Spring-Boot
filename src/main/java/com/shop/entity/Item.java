627845bcdd62ffea96eb960d9c5c8102c5e63062
Jaehyuk <jaehyuk.pyon@gmail.com>
2022-05-19 20:25:16 +0900

    May 19, 2022

---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,43 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Table(name = "item")
+@Entity
+public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String itemNm;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stockNumber;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
---
+++ src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        System.out.println("\r\nMemberService's saveMember(Member member) called...");
+        validateDuplicateMember(member);
+        System.out.println("\r\nMemberService's saveMember(Member member) ended...");
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        System.out.println("\r\nMemberService's validateDuplicateMember(Member member) called...");
+
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+
+        System.out.println("\r\nMemberService's validateDuplicateMember(Member member) ended...");
+    }
+
+}
